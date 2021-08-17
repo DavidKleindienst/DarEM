8b8c5f2dfa53e9ba954b7bf5a62bae9756c09e4b
David Kleindienst <david.kleindienst@ist.ac.at>
2021-08-17 13:49:40 +0200

    Added argumentparser and coordinate output

--- predict.py
+++ predict.py
@@ -6,59 +6,194 @@ Created on Tue Aug 10 15:09:59 2021
 @author: krasax
 """
 
-import os, cv2
+import os, cv2, json
+import argparse
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 
 import time
-from object_detection.utils import label_map_util
+#from object_detection.utils import label_map_util
 from object_detection.utils import config_util
-from object_detection.utils import visualization_utils as viz_utils
 from modifiedObjectDetection import model_builder
 
-import matplotlib
-matplotlib.use('module://ipykernel.pylab.backend_inline')
-plt=matplotlib.pyplot
+#import matplotlib
+#matplotlib.use('module://ipykernel.pylab.backend_inline')
+#plt=matplotlib.pyplot
 
 import utils
 
-PATH_TO_CFG = '/home/krasax/Python_scripts/PSD_Finder/my_models/efficientnet_d2/pipeline.config'
-PATH_TO_CKPT = '/home/krasax/Python_scripts/PSD_Finder/clusterTrainedModels/efficientdet_D2/ckpt-55'
-PATH_TO_LABELS = '/home/krasax/Python_scripts/PSD_Finder/label_map.pbtxt'
-MIN_SCORE = 0.15
+def main(args=None):
 
-output_folder = '/media/krasax/SSD/SerialEM/15E-MS4R-A1-AMPAR_N1/2021-04-30/pred_D2_C55_15Percent'
-image_folder= '/media/krasax/SSD/SerialEM/15E-MS4R-A1-AMPAR_N1/2021-04-30/SR_long'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pipeline_path', type=str, required=True, 
+                        help='Path to the pipeline.config file.')
+    parser.add_argument('--checkpoint_path', type=str, required=True, 
+                        help='Path to the checkpoint file')
+    parser.add_argument('--input_folder', type=str, required=True, 
+                        help='Folder containing the images to be predicted on')
+    parser.add_argument('--min_score', type=float, default=0.15, 
+                        help='Minimum score for a detection to be taken')
+    parser.add_argument('--output_folder', type=str, default=None, 
+                        help='Folder for output images. Keep as None for disabling image output.')
+    parser.add_argument('--coordinate_file', type=str, default=None, 
+                        help='Path where coordinate.json file should be created. Keep as None to not create coordinate file')
+    parser.add_argument('--downscale_targetsize', type=int, nargs='+', default=2048, 
+                        help='Targetsize of image after downscaling')
+    parser.add_argument('--split_targetsize', type=int, nargs='+', default=768,
+                        help='Targetsize of image after splitting')
+    parser.add_argument('--overlap', type=float, default=0.1, 
+                        help='Overlap between split images')
+    
+    if args is None:
+        args=parser.parse_args()
+    else:
+        args=parser.parse_args(args)
+    
+    if args.output_folder is None and args.coordinate_file is None:
+        raise ValueError('Either --output_folder or --coordinate_file has to be set!')
+    
+    if args.coordinate_file:
+        coordinates=dict()
+        if os.path.isdir(args.coordinate_file):
+            args.coordinate_file = os.path.join(args.coordinate_file, 'centroids.json')
+        elif not args.coordinate_file.endswith('.json'):
+            args.coordinate_file += '.json'
+    
+    #PATH_TO_CFG = '/home/krasax/Python_scripts/PSD_Finder/my_models/efficientnet_d2/pipeline.config'
+    #PATH_TO_CKPT = '/home/krasax/Python_scripts/PSD_Finder/clusterTrainedModels/efficientdet_D2/ckpt-55'
+    #PATH_TO_LABELS = '/home/krasax/Python_scripts/PSD_Finder/label_map.pbtxt'
+    #MIN_SCORE = 0.15
+    
+    #output_folder = '/media/krasax/SSD/SerialEM/15E-MS4R-A1-AMPAR_N1/2021-04-30/pred_D2_C55_15Percent'
+    #image_folder= '/media/krasax/SSD/SerialEM/15E-MS4R-A1-AMPAR_N1/2021-04-30/SR_long'
+    
+    if args.output_folder and not os.path.isdir(args.output_folder):
+        os.mkdir(args.output_folder)
+    
+    image_names = [x for x in os.listdir(args.input_folder) if x.endswith('.tif') and not x.startswith('.')
+                   and not x.endswith('_mod.tif') and not x.endswith('_morph.tif')]
+    image_names.sort()
+    
+    #image_names = ['replica0007.tif']
+    
+    image_paths = [os.path.join(args.input_folder,x) for x in image_names]
+    output_paths = [os.path.join(args.output_folder,x) if args.output_folder else None for x in image_names]
+    
+    
+    
+    print('Loading model... ', end='')
+    start_time = time.time()
+    
+    # Load pipeline config and build a detection model
+    configs = config_util.get_configs_from_pipeline_file(args.pipeline_path)
+    model_config = configs['model']
+    detection_model = model_builder.build(model_config=model_config, is_training=False)
 
-if not os.path.isdir(output_folder):
-    os.mkdir(output_folder)
+    # Restore checkpoint
+    ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+    ckpt.restore(args.checkpoint_path).expect_partial()
+    current_time = time.time()
+    print('Done! Took {} seconds'.format(current_time - start_time))
+    #downscale_targetSize = 2048
+    #split_targetSize = 768
+    #overlap = 10
+    for image_path,output_path in zip(image_paths,output_paths):
+    
+        print('Running inference for {}... '.format(image_path), end='')
+    
+        image_np = cv2.imread(image_path)
+        originalSize = image_np.shape
+        image_np = utils.downscaleImage(args.downscale_targetsize, image_np)
+    
+        # Things to try:
+        # Flip horizontally
+        # image_np = np.fliplr(image_np).copy()
+    
+        # Convert image to grayscale
+        # image_np = np.tile(
+        #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+        
+        
+        normImg = np.zeros(image_np.shape)
+        normImg = cv2.normalize(image_np,normImg,0,255,cv2.NORM_MINMAX)
+        
+        images = utils.splitImage(args.split_targetsize, normImg, args.overlap)
+        outImgs = []
+        imCoords = []
+        for im in images:
+            input_tensor = tf.convert_to_tensor(np.expand_dims(im, 0), dtype=tf.float32)
+            
+            
+            detections = detect_fn(input_tensor,detection_model)
+        
+            # All outputs are batches tensors.
+            # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+            # We're only interested in the first num_detections.
+            num_detections = int(detections.pop('num_detections'))
+            detections = {key: value[0, :num_detections].numpy()
+                          for key, value in detections.items()}
+            detections['num_detections'] = num_detections
+        
+            # detection_classes should be ints.
+            detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+        
+            label_id_offset = 1
+            
+            outIm = np.zeros((im.shape[0],im.shape[1]))
+            splitCoords=[]
+            
+            for box in detections['detection_boxes'][detections['detection_scores']>args.min_score]:
+                            
+                ymin, xmin, ymax, xmax = box
+                ymin = round(ymin * im.shape[0])
+                ymax = round(ymax * im.shape[0])     
+                xmin = round(xmin * im.shape[1])
+                xmax = round(xmax * im.shape[1]) 
+                
+                if args.output_folder:
+                    outIm[ymin:ymax,xmin:xmax] = 1
+                
+                    
+                
+                splitCoords.append([xmin+(xmax-xmin)/2, ymin+(ymax-ymin)/2])
+                
+                # plt.figure()
+                # plt.imshow(im)
+                # plt.figure()
+                # plt.imshow(outIm)
+            if args.output_folder:    
+                outImgs.append(outIm)
+            imCoords.append(splitCoords)
+            
 
-image_names = [x for x in os.listdir(image_folder) if x.endswith('.tif') and not x.startswith('.')
-               and not x.endswith('_mod.tif') and not x.endswith('_morph.tif')]
-image_names.sort()
+        if args.output_folder:    
+            assert len(images) == len(outImgs)
+                
+            outImage = utils.fuseMasks(normImg.shape, outImgs, args.overlap)
+            cv2.imwrite(output_path, np.uint8(outImage)*255)
+        
+        if args.coordinate_file:
+            assert len(images) == len(imCoords)
+            
+            imageCoords = utils.get_coords_from_split(normImg.shape, args.split_targetsize,
+                                                         imCoords, args.overlap)
+            k = filepath_to_name(image_path)
+            
+            coordinates[k] = utils.upscale_coordinates(imageCoords, originalSize[0:2], normImg.shape[0:2])
+            
+        print(f'Done. Took {time.time()-current_time} seconds')
+        current_time = time.time()
+        
+    #plt.show()
+    if args.coordinate_file:
+        with open(args.coordinate_file, 'w') as f:
+            json.dump(coordinates,f)
 
-#image_names = ['replica0007.tif']
-
-image_paths = [os.path.join(image_folder,x) for x in image_names]
-output_paths = [os.path.join(output_folder,x) for x in image_names]
-
-
-
-print('Loading model... ', end='')
-start_time = time.time()
-
-# Load pipeline config and build a detection model
-configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
-model_config = configs['model']
-detection_model = model_builder.build(model_config=model_config, is_training=False)
-
-# Restore checkpoint
-ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(PATH_TO_CKPT).expect_partial()
-
+    print(f'Finished. Took {time.time()-start_time} seconds for {len(image_paths)} images.')
+    
 @tf.function
-def detect_fn(image):
+def detect_fn(image,detection_model):
     """Detect objects in image."""
 
     image, shapes = detection_model.preprocess(image)
@@ -66,10 +201,6 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
 
     return detections
-
-end_time = time.time()
-elapsed_time = end_time - start_time
-print('Done! Took {} seconds'.format(elapsed_time))
 
 
 def load_image_into_numpy_array(path):
@@ -87,94 +218,14 @@ def load_image_into_numpy_array(path):
     """
     return np.array(Image.open(path))
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
-                                                                    use_display_name=True)
-downscale_targetSize = 2048
-split_targetSize = 768
-overlap = 10
-for image_path,output_path in zip(image_paths,output_paths):
+def filepath_to_name(full_name,remove_Mod=False):
+    file_name = os.path.basename(full_name)
+    file_name = os.path.splitext(file_name)[0]
+    if remove_Mod and file_name.endswith('_mod'):
+        file_name=file_name[0:-4]
+    return file_name
 
-    print('Running inference for {}... '.format(image_path), end='')
+if __name__=='__main__':
+    
+    main(None)
 
-    image_np = cv2.imread(image_path)
-    image_np = utils.downscaleImage(downscale_targetSize, image_np)
-
-    # Things to try:
-    # Flip horizontally
-    # image_np = np.fliplr(image_np).copy()
-
-    # Convert image to grayscale
-    # image_np = np.tile(
-    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
-    
-    
-    normImg = np.zeros(image_np.shape)
-    normImg = cv2.normalize(image_np,normImg,0,255,cv2.NORM_MINMAX)
-    
-    images= utils.splitImage(split_targetSize, normImg, overlap)
-    outImgs = []
-    for im in images:
-        input_tensor = tf.convert_to_tensor(np.expand_dims(im, 0), dtype=tf.float32)
-        
-        
-        detections = detect_fn(input_tensor)
-    
-        # All outputs are batches tensors.
-        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-        # We're only interested in the first num_detections.
-        num_detections = int(detections.pop('num_detections'))
-        detections = {key: value[0, :num_detections].numpy()
-                      for key, value in detections.items()}
-        detections['num_detections'] = num_detections
-    
-        # detection_classes should be ints.
-        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-    
-        label_id_offset = 1
-        
-        outIm = np.zeros((im.shape[0],im.shape[1]))
-        
-        for box in detections['detection_boxes'][detections['detection_scores']>MIN_SCORE]:
-                        
-            ymin, xmin, ymax, xmax = box
-            ymin = round(ymin * im.shape[0])
-            ymax = round(ymax * im.shape[0])     
-            xmin = round(xmin * im.shape[1])
-            xmax = round(xmax * im.shape[1]) 
-            outIm[ymin:ymax,xmin:xmax] = 1
-        
-        # plt.figure()
-        # plt.imshow(im)
-        # plt.figure()
-        # plt.imshow(outIm)
-        
-        outImgs.append(outIm)
-        
-        
-        # image_np_with_detections = im.copy()
-    
-        # image_np_with_detections=viz_utils.visualize_boxes_and_labels_on_image_array(
-        #         image_np_with_detections,
-        #         detections['detection_boxes'],
-        #         detections['detection_classes']+label_id_offset,
-        #         detections['detection_scores'],
-        #         category_index,
-        #         use_normalized_coordinates=True,
-        #         max_boxes_to_draw=50,
-        #         min_score_thresh=.15,
-        #         agnostic_mode=False)
-    
-        # plt.figure()
-        # plt.imshow(image_np_with_detections)
-    
-    assert len(images) == len(outImgs)
-        
-    outImage = utils.fuseMasks(normImg.shape, outImgs, overlap)
-    cv2.imwrite(output_path, np.uint8(outImage)*255)
-    # plt.figure()
-    # plt.imshow(normImg)
-    # plt.figure()
-    # plt.imshow(outImage)
-    
-    print('Done')
-plt.show()
--- utils.py
+++ utils.py
@@ -25,8 +25,7 @@ def normalizeImage(image):
 def downscaleImage(targetSize, image, mask=None):
     
     if targetSize:
-        if type(targetSize) is int:
-            targetSize=(targetSize, targetSize)
+        targetSize = correctTargetSize(targetSize)
         if targetSize[0]<image.shape[0] or targetSize[1]<image.shape[1]:
             image = cv2.resize(image,(targetSize[0],targetSize[1]),interpolation=cv2.INTER_CUBIC)
             if mask:
@@ -38,20 +37,18 @@ def downscaleImage(targetSize, image, mask=None):
         return image
     
     
-def splitImage(targetSize, image, overlap=0):
-    
+def splitImage(targetSize, image, overlap=0):    
     if not targetSize:
         return [image]
-    if type(targetSize) is int:
-        targetSize = (targetSize, targetSize)
+    targetSize = correctTargetSize(targetSize)
     if targetSize[0]>=image.shape[0] and targetSize[1] >= image.shape[1]:
         return [image]
-    
+
     overlap = correctOverlap(overlap)
     
     ystarts, yends = getSplitIndices(image.shape[0], targetSize[0], overlap)
     xstarts, xends = getSplitIndices(image.shape[1], targetSize[1], overlap)
-    
+
     images = []
     for ys, ye in zip(ystarts, yends):
         for xs, xe in zip(xstarts, xends):
@@ -64,6 +61,55 @@ def splitImage(targetSize, image, overlap=0):
                 raise ValueError(f'Image should have two or three dimensions. Got {len(image.shape)}.')
                 
     return images
+
+def correctTargetSize(targetSize):
+    if type(targetSize) is int:
+        targetSize=(targetSize, targetSize)
+    elif len(targetSize)==1:
+        targetSize=(targetSize[0], targetSize[0])
+    elif len(targetSize)>2:
+        targetSize=targetSize[0:2]
+    return targetSize
+
+def upscale_coordinates(coords,targetShape,currentShape):
+    if not coords:
+        return coords
+    if not currentShape: 
+        return [[round(x),round(y)] for (x,y) in coords]
+    currentShape = correctTargetSize(currentShape)
+    if currentShape == targetShape:
+        return [[round(x),round(y)] for (x,y) in coords]
+        
+    yratio = targetShape[0] / currentShape[0]
+    xratio = targetShape[1] / currentShape[1]
+    
+    upscaled = [[round(xratio * x), round(yratio * y)] for (x,y) in coords]
+    
+    return upscaled
+
+def get_coords_from_split(targetShape, splitShape, coords, overlap=0):
+    if len(coords) == 1:
+        return coords[0]
+    splitShape = correctTargetSize(splitShape)
+    overlap = correctOverlap(overlap)
+    ystarts, _ = getSplitIndices(targetShape[0], splitShape[0], overlap)
+    xstarts, _ = getSplitIndices(targetShape[1], splitShape[1], overlap)
+    
+    assert len(coords) == len(xstarts) * len(ystarts)
+    
+    coordinates = []
+    count=0
+    for ys in ystarts:
+        for xs in xstarts:
+            coord = coords[count]
+            
+            if coord: #No need to do anything if it is empty list
+                conv_coords = [[xs+x, ys+y] for (x,y) in coord]
+                coordinates+=conv_coords
+                
+            count+=1
+            
+    return coordinates
 
 def fuseMasks(targetShape, images, overlap=0):
     if len(images) == 1:
@@ -78,7 +124,6 @@ def fuseMasks(targetShape, images, overlap=0):
     #If there is an overlap of 10%, first each image should fill 5% of that overlap
     #Eg. 1st image goes from 0 to 1000 and 2nd image goes from 900 to 1900
     #Then first image should fill 0->950 and 2nd image should fill from 950
-    
     
     
     outputImage = np.zeros((targetShape[0],targetShape[1])) #Mask is Grayscale
